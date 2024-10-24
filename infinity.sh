7ef3ff1738559e46e97dec244a7184817646f30e
K4LCHAKRA <oseverest44@gmail.com>
2024-10-24 08:34:59 +0000

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -9,9 +9,6 @@ repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/man
 # Sync the rpo with force to ensure a clean sync
 /opt/crave/resync.sh
 
-# remove 
-rm -rf packages/apps/GameSpace
-
 # cloning device tree
 git clone https://github.com/infinity-mojito/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
 git clone https://github.com/matrix-mojito/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
@@ -25,12 +22,6 @@ git clone https://gitlab.com/bliss-mojito/android_vendor_xiaomi_sm6150-common.gi
 
 # cloning hardware tree
 git clone https://github.com/bliss-mojito/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-
-#cloning private keys
-git clone https://github.com/sunny-keys/vendor_lineage-priv_keys-matrixOS.git --depth 1 -b master vendor/lineage-priv/keys
-
-# add source file
-git clone https://github.com/ProjectMatrixx/android_packages_apps_GameSpace.git --depth 1 -b 14.0 packages/apps/GameSpace
 
 # Set up the build environment
 . build/envsetup.sh
