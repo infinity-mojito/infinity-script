7ec6220783cd0b0e7ec5d278d0d24fce9dd672ec
K4LCHAKRA <oseverest44@gmail.com>
2024-10-24 08:30:11 +0000

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -13,7 +13,7 @@ repo init -u https://github.com/ProjectMatrixx/android.git -b 14.0 --git-lfs
 rm -rf packages/apps/GameSpace
 
 # cloning device tree
-git clone https://github.com/matrix-mojito/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
+git clone https://github.com/infinity-mojito/device_xiaomi_mojito.git --depth 1 -b mojito-universe device/xiaomi/mojito
 git clone https://github.com/matrix-mojito/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
 
 # cloning kernel tree
