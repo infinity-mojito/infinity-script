7470bc87dc5ef9cf10a86268ad8542b9f61a4608
K4LCHAKRA <oseverest44@gmail.com>
2025-01-02 23:28:15 +0545

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# remove device tree
+# before clone remove
 rm -rf device/xiaomi/mojito
 rm -rf device/xiaomi/sm6150-common
 rm -rf kernel/xiaomi/mojito
 rm -rf vendor/xiaomi/sm6150-common
 rm -rf hardware/xiaomi
+rm -rf vendor/xiaomi/mojito-leicacamera
+rm -rf vendor/xiaomi/miuiapps
 
 rm -rf frameworks/native
 rm -rf frameworks/av
@@ -17,6 +19,7 @@ repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/man
 # Sync the repo with force to ensure a clean sync
 /opt/crave/resync.sh
 
+# after clone remove
 rm -rf device/xiaomi/mojito
 rm -rf device/xiaomi/sm6150-common
 rm -rf kernel/xiaomi/mojito
@@ -25,6 +28,7 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/mojito-leicacamera
 rm -rf vendor/xiaomi/miuiapps
 
+# remove source
 rm -rf vendor/infinity
 rm -rf frameworks/av
 
@@ -42,10 +46,8 @@ git clone https://gitlab.com/romgharti/android_vendor_xiaomi_sm6150-common.git -
 # cloning hardware tree
 git clone https://github.com/ProjectEverest-Devices/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
 
-# clone vendor/infinity
+# add source
 git clone https://github.com/infinity-mojito/vendor_infinity.git -b 15 vendor/infinity
-
-rm -rf frameworks/av
 git clone https://github.com/infinity-mojito/frameworks_av.git -b 15 frameworks/av
 
 git clone https://gitlab.com/dpenra/android_vendor_xiaomi_mojito-leicacamera.git --depth 1 -b main vendor/xiaomi/mojito-leicacamera
@@ -60,6 +62,7 @@ lunch infinity_mojito-user
 # Build the ROM (use mka bacon for a full build)
 mka bacon
 
+# pixeldrain upload
 cd out/target/product/mojito
 curl -T Project_Infinity*.zip -u :2b0faa06-cdf4-45b2-bb25-34e443e133ce https://pixeldrain.com/api/file/
 curl -T Project_Infinity*.zip -u :df7faee7-526a-4d50-bbee-e8e9fb041f3d https://pixeldrain.com/api/file/
